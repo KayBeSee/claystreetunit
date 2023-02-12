f82213a0b85be8152df41d272b488e31d501150b
Kevin Mulcrone <KayBeSee@gmail.com>
2023-02-12 09:28:36 -0700

    remove font array

--- apps/website/pages/api/og.tsx
+++ apps/website/pages/api/og.tsx
@@ -125,18 +125,6 @@ export default async function handler(req: NextRequest) {
     {
       width: 1200,
       height: 630,
-      fonts: [
-        // {
-        //   name: 'Raleway',
-        //   data: ralewayMedium,
-        //   style: 'normal',
-        // },
-        // {
-        //   name: 'Raleway-Bold',
-        //   data: ralewayBold,
-        //   style: 'normal',
-        // },
-      ],
     }
   );
 }
