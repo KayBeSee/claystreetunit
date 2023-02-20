3fb18aeba83bb71d14c96e1eb8500d86027158e5
Kevin Mulcrone <KayBeSee@gmail.com>
2023-02-20 16:35:03 -0700

    include in tailwind config

--- apps/website/tailwind.config.js
+++ apps/website/tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
-    './data/**.ts',
+    '../../data/**.ts',
     '../../packages/components/**/*.{js,ts,jsx,tsx}',
   ],
   // safelist: safelist,
