e742d267374c896e61b8e88a14d48b41a07c5c2c
Kevin Mulcrone <KayBeSee@gmail.com>
2023-02-21 16:10:37 -0700

    fix: ToBT links

--- data/music.ts
+++ data/music.ts
@@ -46,12 +46,11 @@ export const music: Props = {
     description: `This is filler text if we want to put a description of the record here. Don't leave your records in the sun. They'll warp and won't be any good for anyone. Don't leave your records in the sun. They get all wavy and they just won't run. They just won't play, just won't play, just won't play. Just won't play, just won't play, just won't play. Just won't play, just won't play, just won't play. Just won't play, just won't play no more`,
     otherImages: [],
     streamingLinks: {
-      spotify: "https://open.spotify.com/album/3HhQMg7ob3cp5NayDVmQsF",
-      apple:
-        "https://music.apple.com/us/album/a-mighty-fine-evening-ep/1649821369",
-      amazon: "https://music.amazon.com/albums/B0BJ4WKBRK",
+      spotify: "https://open.spotify.com/album/2OkUijeVAx9Pu51NlQkqzw",
+      apple: "https://music.apple.com/us/album/tired-of-being-tired/1648148757",
+      amazon: "https://music.amazon.com/albums/B0BH65JYLX",
       youtube:
-        "https://music.youtube.com/playlist?list=OLAK5uy_kqfqpp40ssYy-fw6aAhEkWg7_JMk_l4cY",
+        "https://music.youtube.com/playlist?list=OLAK5uy_kIq8NsYswJOeGh6Rg-5DA7mRu_aMrxBXU",
     },
     pageStyle: {
       streamServiceColor: "fill-[#60a5fa]",
